187e9efa677e5ea43e99262184264b224ab4a330
Ferdi ÜNAL <ferdiunal@users.noreply.github.com>
2016-02-26 15:45:10 +0200

    Update starts.sh

--- starts.sh
+++ starts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo service beanstalkd start
-sudo service memcached  start
-sudo service nginx      start
-sudo service php7.0-fpm start
+sudo service beanstalkd   start
+sudo service memcached    start
+sudo service nginx        start
+sudo service php7.0-fpm   start
 sudo service redis-server start
